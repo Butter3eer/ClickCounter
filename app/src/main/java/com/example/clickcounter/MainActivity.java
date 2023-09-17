e7d6008abb0765623552895ac0c2deb48eb74a02
Butter3eer <palcsenge2015@gmail.com>
2023-09-17 19:59:30 +0200

    ongoing work

--- app/src/main/java/com/example/clickcounter/MainActivity.java
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonPlus;
     private Button buttonMinus;
     private int szamlalo;
+    private RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,29 +37,47 @@ public class MainActivity extends AppCompatActivity {
             colorCheck();
         });
 
-        textViewSzamlalo.setOnClickListener(view -> {
+        textViewSzamlalo.setOnLongClickListener(view -> {
             szamlalo = 0;
             textViewSzamlalo.setText(String.valueOf(szamlalo));
             colorCheck();
+            return true;
         });
+
+        layout.getDisplay().getOrientation();
     }
 
     public void init() {
         textViewSzamlalo = findViewById(R.id.textViewSzamlalo);
         buttonPlus = findViewById(R.id.buttonPlus);
         buttonMinus = findViewById(R.id.buttonMinus);
+        layout = findViewById(R.id.relativeLayout);
         szamlalo = 0;
     }
 
     public void colorCheck() {
-        if (szamlalo > 0) {
+        if (!isPrime()) {
+            textViewSzamlalo.setTextColor(getResources().getColor(R.color.white));
+        }
+        else if (szamlalo > 0) {
             textViewSzamlalo.setTextColor(Color.rgb(0, 128, 0));
         }
         else if (szamlalo < 0) {
             textViewSzamlalo.setTextColor(Color.rgb(255, 0, 0));
         }
-        else if (szamlalo == 0) {
+        if (szamlalo == 0) {
             textViewSzamlalo.setTextColor(Color.rgb(61,0, 92));
         }
+    }
+
+    public boolean isPrime() {
+        int szam = Math.abs(szamlalo);
+
+        for (int i = 2; i <= szam / 2; ++i) {
+            if (szam % i == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
